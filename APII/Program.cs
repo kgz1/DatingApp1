20c84c37e81f244649de343b2493436a7c4e2b6a
kg z1 <kg034269@gmail.com>
2023-10-01 23:43:33 +0200

    End of section 3

--- APII/Program.cs
+++ APII/Program.cs
@@ -1,6 +1,7 @@
 using APII.Data;
 using Microsoft.EntityFrameworkCore;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 
@@ -10,10 +11,13 @@ builder.Services.AddDbContext<DataContext> (opt =>
 {
 opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseHttpsRedirection();
 
@@ -22,3 +26,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
